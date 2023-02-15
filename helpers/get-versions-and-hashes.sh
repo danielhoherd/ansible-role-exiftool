4bd7f8db61412740c07ffd917a6ecd4ed3d8edea
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-02-15 15:38:00 -0500

    Quote string with variable

--- helpers/get-versions-and-hashes.sh
+++ helpers/get-versions-and-hashes.sh
@@ -3,9 +3,9 @@
 [[ "${OSTYPE}" =~ ^linux ]] || { echo "We need linux for gnu-stat." ; exit 1 ; }
 
 # https://exiftool.org/history.html
-for X in 12.{35..50} ; do
+for X in 12.{42..60} ; do
   grep "\s\+$X:" "${0%/*}/../vars/main.yml" && continue
   file="Image-ExifTool-$X.tar.gz"
-  wget -q https://exiftool.org/$file || continue
+  wget -q "https://exiftool.org/$file" || continue
   echo "$X: $(sha1sum "$file" | awk '{print $1}') # $(stat -c "%y" "$file" | awk '{print $1}')"
 done
