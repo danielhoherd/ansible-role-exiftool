6afc350a658e52f9cec2cf4dfee8afe6b344b2cb
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-10-29 16:16:14 -0400

    Update hashes. Update default to 13.00

--- helpers/get-versions-and-hashes.sh
+++ helpers/get-versions-and-hashes.sh
@@ -3,9 +3,10 @@
 [[ "${OSTYPE}" =~ ^linux ]] || { echo "We need linux for gnu-stat." ; exit 1 ; }
 
 # https://exiftool.org/history.html
-for X in 12.{84..90} ; do
+for X in 13.{00..02} ; do
   grep "\s\+$X:" "${0%/*}/../vars/main.yml" && continue
   file="Image-ExifTool-$X.tar.gz"
   wget -q "https://exiftool.org/$file" || continue
   echo "\"$X\": $(sha1sum "$file" | awk '{print $1}') # $(stat -c "%y" "$file" | awk '{print $1}')"
+  echo "Sleeping for 65 seconds to avoid being banned" && sleep 65
 done
